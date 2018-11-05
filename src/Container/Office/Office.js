7140ad2e5987a92f05fbfe8d094679dcc5bb5e5d
Gadani Himan Gurusinga <gadanihiman@gmail.com>
2018-11-05 15:22:31 +0700

    Add Page Not Found Condition

--- src/Container/Office/Office.js
+++ src/Container/Office/Office.js
@@ -5,8 +5,13 @@ import { withRouter } from 'react-router-dom';
 class Office extends Component {
   render() {
     return (
+      //check if location state is undefine will show page not found
       <div>
-        <Companies items='office' />
+        {this.props.location.state
+          ? <Companies items='office' />
+          : <h1 style={{ color: 'red' }}> 
+              Page Not Found
+            </h1>}
       </div>
     );
   }
