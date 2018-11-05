b19a478132979ace170f280c4cea935c9ccdd807
Gadani Himan Gurusinga <gadanihiman@gmail.com>
2018-11-05 15:22:06 +0700

    Delete Header React Standart

--- src/App.js
+++ src/App.js
@@ -16,10 +16,7 @@ class App extends Component {
       <Provider store={store}>
         <BrowserRouter>
           <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
+            
             <Route path='/' exact component={Company}/>
             <Route path='/office' exact component={Office}/>
           </div>
